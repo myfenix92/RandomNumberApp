01943ea2403b1511c59529d797a286738bc214b0
Yana Korneykova <bloom.hog@gmail.com>
2023-07-12 17:33:42 +0300

    feat: init

---
+++ app/src/main/java/com/hfad/randomnumber/MainActivity.java
@@ -0,0 +1,37 @@
+package com.hfad.randomnumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        TextView textView = findViewById(R.id.countNumber);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                textView.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if(seekBar.getProgress() < 1)
+                    seekBar.setProgress(1);
+            }
+        });
+    }
+}
